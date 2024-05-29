197b3edda641562ec0bca030b2e084aba314f439
avmentzer <74241215+avmentzer@users.noreply.github.com>
2024-05-29 09:16:54 +0000

    #01_fundamentals completed

---
+++ coding/01_fundamentals/SimpleCalculator/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Hello");
+
+Console.WriteLine("Input the first number");
+int input1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number");
+int input2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do?");
+
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubtract numbers");
+Console.WriteLine("[M]ultiply numbers");
+
+string userChoice = Console.ReadLine().ToUpper();
+
+if (userChoice == "A")
+{
+    calculate(input1, input2, "+");
+}
+
+else if (userChoice == "S")
+{
+    calculate(input1, input2, "-");
+}
+
+else if (userChoice == "M")
+{
+    calculate(input1, input2, "*");
+}
+
+else
+{
+    System.Console.WriteLine("Invalid operation.");
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+void calculate(int input1, int input2, string operand)
+{
+    switch (operand)
+    {
+        case "+":
+            System.Console.WriteLine($"{input1} {operand} {input2} = {input1 + input2}");
+            break;
+        case "-":
+            System.Console.WriteLine($"{input1} {operand} {input2} = {input1 - input2}");
+            break;
+        case "*":
+            System.Console.WriteLine($"{input1} {operand} {input2} = {input1 * input2}");
+            break;
+        default:
+            break;
+    }
+
+}
--- coding/01_fundamentals/ToDoList/Program.cs
+++ coding/01_fundamentals/ToDoList/Program.cs
@@ -1,1 +1,142 @@
-﻿Console.WriteLine("Hello");
+﻿Console.WriteLine("Hello, what do you want to do?");
+PrintAllOptions();
+
+List<string> todoList = new List<string>();
+
+while (true)
+{
+    string userChoice = Console.ReadLine().ToUpper();
+
+    switch (userChoice)
+    {
+        case "S":
+            PrintSelectedOption("See all TODOs");
+            PrintTodos();
+            continue;
+
+        case "A":
+            PrintSelectedOption("Add a TODO");
+
+            while (true)
+            {
+                System.Console.WriteLine("Enter the TODO description:");
+                string addTodo = Console.ReadLine();
+
+                if (todoList.Contains(addTodo))
+                {
+                    Console.WriteLine("The description must be unique.");
+                }
+
+                else if (!string.IsNullOrWhiteSpace(addTodo))
+                {
+                    todoList.Add(addTodo);
+                    System.Console.WriteLine($"TODO successfully added: {addTodo}");
+                    PrintAllOptions();
+                    break;
+                }
+                else
+                {
+                    System.Console.WriteLine("The description cannot be empty.");
+                }
+                continue;
+            }
+            continue;
+
+        case "R":
+            PrintSelectedOption("Remove a TODO");
+            System.Console.WriteLine("Select the index of the TODO you want to remove:");
+
+            PrintTodos(false);
+
+            string userInput = Console.ReadLine();
+
+            while (true)
+            {
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Selected index cannot be empty. Select the index of the TODO you want to remove:");
+                    PrintTodos(false);
+                    userInput = Console.ReadLine();
+                }
+                else
+                {
+                    int index;
+                    bool readIndex = int.TryParse(userInput, out index);
+                    if (readIndex)
+                    {
+                        try
+                        {
+                            System.Console.WriteLine($"TODO removed: {todoList[index - 1]}");
+                            RemoveTodo(index);
+                            PrintAllOptions();
+                            break;
+                        }
+                        catch (ArgumentOutOfRangeException ex)
+                        {
+                            handleException();
+                            userInput = Console.ReadLine();
+                        }
+
+                    }
+                    else
+                    {
+                        handleException();
+                        userInput = Console.ReadLine();
+                    }
+                }
+            }
+            continue;
+
+        case "E":
+            PrintSelectedOption("Exit");
+            return;
+
+        default:
+            Console.WriteLine("Invalid choice. Please enter S, A, R, or E.");
+            continue; // Repeat the loop
+    }
+}
+
+void PrintSelectedOption(string option)
+{
+    Console.WriteLine("Selected option: " + option);
+}
+
+void PrintAllOptions()
+{
+    Console.WriteLine("What else do you want to do?");
+    Console.WriteLine("[S]ee all Todos");
+    Console.WriteLine("[A]dd a Todo");
+    Console.WriteLine("[R]emove a Todo");
+    Console.WriteLine("[E]xit");
+}
+
+void PrintTodos(bool printOptions = true)
+{
+    for (int i = 0; i < todoList.Count; i++)
+    {
+        System.Console.WriteLine($"{i + 1}. " + todoList[i]);
+
+    }
+    if (printOptions && todoList.Count > 0)
+    {
+        PrintAllOptions();
+    }
+    if (todoList.Count == 0)
+    {
+        System.Console.WriteLine("No TODOs have been added yet.");
+        PrintAllOptions();
+    }
+}
+
+void RemoveTodo(int index)
+{
+    todoList.RemoveAt(index - 1);
+}
+
+void handleException()
+{
+    System.Console.WriteLine("The given index is not valid. Select the index of the TODO you want to remove:");
+    PrintTodos(false);
+
+}
