84558695a0a910e1a9225da59ffa6ae3df888e9a
avmentzer <74241215+avmentzer@users.noreply.github.com>
2024-05-30 08:57:30 +0000

    #01_fundamentals ToDoList assignment refactoring

--- coding/01_fundamentals/ToDoList/Program.cs
+++ coding/01_fundamentals/ToDoList/Program.cs
@@ -1,10 +1,10 @@
-﻿Console.WriteLine("Hello, what do you want to do?");
-PrintAllOptions();
-
-List<string> todoList = new List<string>();
+﻿Console.WriteLine("Start of Program");
+List<string> todoList = [];
 
 while (true)
 {
+    PrintAllOptions();
+
     string userChoice = Console.ReadLine().ToUpper();
 
     switch (userChoice)
@@ -12,80 +12,17 @@ while (true)
         case "S":
             PrintSelectedOption("See all TODOs");
             PrintTodos();
-            continue;
+            break;
 
         case "A":
             PrintSelectedOption("Add a TODO");
-
-            while (true)
-            {
-                System.Console.WriteLine("Enter the TODO description:");
-                string addTodo = Console.ReadLine();
-
-                if (todoList.Contains(addTodo))
-                {
-                    Console.WriteLine("The description must be unique.");
-                }
-
-                else if (!string.IsNullOrWhiteSpace(addTodo))
-                {
-                    todoList.Add(addTodo);
-                    System.Console.WriteLine($"TODO successfully added: {addTodo}");
-                    PrintAllOptions();
-                    break;
-                }
-                else
-                {
-                    System.Console.WriteLine("The description cannot be empty.");
-                }
-                continue;
-            }
-            continue;
+            addTodo();
+            break;
 
         case "R":
             PrintSelectedOption("Remove a TODO");
-            System.Console.WriteLine("Select the index of the TODO you want to remove:");
-
-            PrintTodos(false);
-
-            string userInput = Console.ReadLine();
-
-            while (true)
-            {
-                if (string.IsNullOrWhiteSpace(userInput))
-                {
-                    Console.WriteLine("Selected index cannot be empty. Select the index of the TODO you want to remove:");
-                    PrintTodos(false);
-                    userInput = Console.ReadLine();
-                }
-                else
-                {
-                    int index;
-                    bool readIndex = int.TryParse(userInput, out index);
-                    if (readIndex)
-                    {
-                        try
-                        {
-                            System.Console.WriteLine($"TODO removed: {todoList[index - 1]}");
-                            RemoveTodo(index);
-                            PrintAllOptions();
-                            break;
-                        }
-                        catch (ArgumentOutOfRangeException ex)
-                        {
-                            handleException();
-                            userInput = Console.ReadLine();
-                        }
-
-                    }
-                    else
-                    {
-                        handleException();
-                        userInput = Console.ReadLine();
-                    }
-                }
-            }
-            continue;
+            removeTado();
+            break;
 
         case "E":
             PrintSelectedOption("Exit");
@@ -93,7 +30,7 @@ while (true)
 
         default:
             Console.WriteLine("Invalid choice. Please enter S, A, R, or E.");
-            continue; // Repeat the loop
+            break;
     }
 }
 
@@ -104,39 +41,104 @@ void PrintSelectedOption(string option)
 
 void PrintAllOptions()
 {
-    Console.WriteLine("What else do you want to do?");
+    Console.WriteLine("What do you want to do?");
     Console.WriteLine("[S]ee all Todos");
     Console.WriteLine("[A]dd a Todo");
     Console.WriteLine("[R]emove a Todo");
     Console.WriteLine("[E]xit");
 }
 
-void PrintTodos(bool printOptions = true)
+void PrintTodos()
 {
-    for (int i = 0; i < todoList.Count; i++)
-    {
-        System.Console.WriteLine($"{i + 1}. " + todoList[i]);
-
-    }
-    if (printOptions && todoList.Count > 0)
-    {
-        PrintAllOptions();
-    }
     if (todoList.Count == 0)
     {
-        System.Console.WriteLine("No TODOs have been added yet.");
-        PrintAllOptions();
+        Console.WriteLine("No TODOs have been added yet.");
     }
-}
+    else
+    {
+        for (int i = 0; i < todoList.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. " + todoList[i]);
+        }
+    }
 
-void RemoveTodo(int index)
-{
-    todoList.RemoveAt(index - 1);
 }
 
 void handleException()
 {
-    System.Console.WriteLine("The given index is not valid. Select the index of the TODO you want to remove:");
-    PrintTodos(false);
+    Console.WriteLine("The given index is not valid. Select the index of the TODO you want to remove:");
+    PrintTodos();
+}
 
+void addTodo()
+{
+    while (true)
+    {
+        Console.WriteLine("Enter the TODO description:");
+        string addTodo = Console.ReadLine();
+
+        if (todoList.Contains(addTodo))
+        {
+            Console.WriteLine("The description must be unique.");
+        }
+
+        else if (!string.IsNullOrWhiteSpace(addTodo))
+        {
+            todoList.Add(addTodo);
+            Console.WriteLine($"TODO successfully added: {addTodo}");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("The description cannot be empty.");
+        }
+    }
+}
+
+void removeTado()
+{
+    if (todoList.Count == 0)
+    {
+        Console.WriteLine("No TODOs have been added yet.");
+        return;
+    }
+
+    Console.WriteLine("Select the index of the TODO you want to remove:");
+    PrintTodos();
+
+    string userInput = Console.ReadLine();
+
+    while (true)
+    {
+        if (string.IsNullOrWhiteSpace(userInput))
+        {
+            Console.WriteLine("Selected index cannot be empty. Select the index of the TODO you want to remove:");
+            PrintTodos();
+            userInput = Console.ReadLine();
+        }
+        else
+        {
+            bool readIndex = int.TryParse(userInput, out int index);
+            if (readIndex)
+            {
+                try
+                {
+                    Console.WriteLine($"TODO removed: {todoList[index - 1]}");
+                    todoList.RemoveAt(index - 1);
+                    break;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    handleException();
+                    userInput = Console.ReadLine();
+                }
+
+            }
+            else
+            {
+                handleException();
+                userInput = Console.ReadLine();
+            }
+        }
+    }
 }
